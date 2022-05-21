16ac2db6d3061b791293e99df1e9517d3e6abc09
Brendan Hasz <winsto99@gmail.com>
2022-05-20 20:13:42 -0500

    use simpler bar chart

--- assets/js/bar_chart.js
+++ assets/js/bar_chart.js
@@ -1,9 +1,10 @@
 // Copyright 2021 Observable, Inc.
 // Released under the ISC license.
-// https://observablehq.com/@d3/sortable-bar-chart
+// https://observablehq.com/@d3/bar-chart
 function BarChart(data, {
   x = (d, i) => i, // given d in data, returns the (ordinal) x-value
   y = d => d, // given d in data, returns the (quantitative) y-value
+  title, // given d in data, returns the title text
   marginTop = 20, // the top margin, in pixels
   marginRight = 0, // the right margin, in pixels
   marginBottom = 30, // the bottom margin, in pixels
@@ -12,16 +13,13 @@ function BarChart(data, {
   height = 400, // the outer height of the chart, in pixels
   xDomain, // an array of (ordinal) x-values
   xRange = [marginLeft, width - marginRight], // [left, right]
-  yType = d3.scaleLinear, // type of y-scale
+  yType = d3.scaleLinear, // y-scale type
   yDomain, // [ymin, ymax]
   yRange = [height - marginBottom, marginTop], // [bottom, top]
   xPadding = 0.1, // amount of x-range to reserve to separate bars
   yFormat, // a format specifier string for the y-axis
   yLabel, // a label for the y-axis
-  //color = "currentColor", // bar fill color
   color = d => d, // given d in data, returns the color
-  duration: initialDuration = 250, // transition duration, in milliseconds
-  delay: initialDelay = (_, i) => i * 20 // per-element transition delay, in milliseconds
 } = {}) {
   // Compute values.
   const X = d3.map(data, x);
@@ -41,7 +39,16 @@ function BarChart(data, {
   const yScale = yType(yDomain, yRange);
   const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
   const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
-  const format = yScale.tickFormat(100, yFormat);
+
+  // Compute titles.
+  if (title === undefined) {
+    const formatValue = yScale.tickFormat(100, yFormat);
+    title = i => `${X[i]}\n${formatValue(Y[i])}`;
+  } else {
+    const O = d3.map(data, d => d);
+    const T = title;
+    title = i => T(O[i], i, data);
+  }
 
   const svg = d3.create("svg")
       .attr("width", width)
@@ -49,11 +56,13 @@ function BarChart(data, {
       .attr("viewBox", [0, 0, width, height])
       .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
-  const yGroup = svg.append("g")
+  svg.append("g")
       .attr("transform", `translate(${marginLeft},0)`)
       .call(yAxis)
       .call(g => g.select(".domain").remove())
-      .call(g => g.selectAll(".tick").call(grid))
+      .call(g => g.selectAll(".tick line").clone()
+          .attr("x2", width - marginLeft - marginRight)
+          .attr("stroke-opacity", 0.1))
       .call(g => g.append("text")
           .attr("x", -marginLeft)
           .attr("y", 10)
@@ -61,106 +70,22 @@ function BarChart(data, {
           .attr("text-anchor", "start")
           .text(yLabel));
 
-  let rect = svg.append("g")
+  const bar = svg.append("g")
     .selectAll("rect")
-    //.attr("fill", color)
     .data(I)
     .join("rect")
-      .property("key", i => X[i]) // for future transitions
-      .call(position, i => xScale(X[i]), i => yScale(Y[i]))
-      .style("mix-blend-mode", "multiply")
-      .attr("fill", i => C[i])
-      .call(rect => rect.append("title")
-          .text(i => [X[i], format(Y[i])].join("\n")));
+    .attr("fill", i => C[i])
+    .attr("x", i => xScale(X[i]))
+    .attr("y", i => yScale(Y[i]))
+    .attr("height", i => yScale(0) - yScale(Y[i]))
+    .attr("width", xScale.bandwidth());
 
-  const xGroup = svg.append("g")
+  if (title) bar.append("title")
+      .text(title);
+
+  svg.append("g")
       .attr("transform", `translate(0,${height - marginBottom})`)
       .call(xAxis);
 
-  // A helper method for updating the position of bars.
-  function position(rect, x, y) {
-    return rect
-        .attr("x", x)
-        .attr("y", y)
-        .attr("height", typeof y === "function" ? i => yScale(0) - y(i) : i => yScale(0) - y)
-        .attr("width", xScale.bandwidth());
-  }
-
-  // A helper method for generating grid lines on the y-axis.
-  function grid(tick) {
-    return tick.append("line")
-        .attr("class", "grid")
-        .attr("x2", width - marginLeft - marginRight)
-        .attr("stroke", "currentColor")
-        .attr("stroke-opacity", 0.1);
-  }
-
-  // Call chart.update(data, options) to transition to new data.
-  return Object.assign(svg.node(), {
-    update(data, {
-      xDomain, // an array of (ordinal) x-values
-      yDomain, // [ymin, ymax]
-      duration = initialDuration, // transition duration, in milliseconds
-      delay = initialDelay // per-element transition delay, in milliseconds
-    } = {}) {
-      // Compute values.
-      const X = d3.map(data, x);
-      const Y = d3.map(data, y);
-
-      // Compute default domains, and unique the x-domain.
-      if (xDomain === undefined) xDomain = X;
-      if (yDomain === undefined) yDomain = [0, d3.max(Y)];
-      xDomain = new d3.InternSet(xDomain);
-
-      // Omit any data not present in the x-domain.
-      const I = d3.range(X.length).filter(i => xDomain.has(X[i]));
-
-      // Update scale domains.
-      xScale.domain(xDomain);
-      yScale.domain(yDomain);
-
-      // Start a transition.
-      const t = svg.transition().duration(duration);
-
-      // Join the data, applying enter and exit.
-      rect = rect
-          .data(I, function(i) { return this.tagName === "rect" ? this.key : X[i]; })
-          .join(
-            enter => enter.append("rect")
-                .property("key", i => X[i]) // for future transitions
-                .call(position, i => xScale(X[i]), yScale(0))
-                .style("mix-blend-mode", "multiply")
-                .call(enter => enter.append("title")),
-            update => update,
-            exit => exit.transition(t)
-                .delay(delay)
-                .attr("y", yScale(0))
-                .attr("height", 0)
-                .remove()
-          );
-
-      // Update the title text on all entering and updating bars.
-      rect.select("title")
-          .text(i => [X[i], format(Y[i])].join("\n"));
-
-      // Transition entering and updating bars to their new position. Note
-      // that this assumes that the input data and the x-domain are in the
-      // same order, or else the ticks and bars may have different delays.
-      rect.transition(t)
-          .delay(delay)
-          .call(position, i => xScale(X[i]), i => yScale(Y[i]));
-
-      // Transition the x-axis (using a possibly staggered delay per tick).
-      xGroup.transition(t)
-          .call(xAxis)
-          .call(g => g.selectAll(".tick").delay(delay));
-
-      // Transition the y-axis, then post process for grid lines etc.
-      yGroup.transition(t)
-          .call(yAxis)
-        .selection()
-          .call(g => g.select(".domain").remove())
-          .call(g => g.selectAll(".tick").selectAll(".grid").data([,]).join(grid));
-    }
-  });
+  return svg.node();
 }
